cf7a75cdde8f68deb2f9f60d3801e3b5b2905fc9
Elizabeth Harper <foxcapade@gmail.com>
2020-10-18 16:24:24 -0400

    added allowed mentions interface

---
+++ pkg/discord/message/AllowedMentions.go
@@ -0,0 +1,32 @@
+package message
+
+import "github.com/foxcapades/lib-go-discord/pkg/dlib"
+
+type AllowedMentions interface {
+	// Parse returns the current value of this record's `parse` field.
+	//
+	// The `parse` field contains an array of allowed mention types to parse from
+	// the content.
+	Parse() []AllowedMentionType
+
+	// SetParse overwrites the current value of this record's `parse` field.
+	SetParse([]AllowedMentionType) AllowedMentions
+
+	// Roles returns the current value of this record's `roles` field.
+	//
+	// The `roles` field contains an array of role_ids to mention (Max size of
+	// 100).
+	Roles() []dlib.Snowflake
+
+	// SetRoles overwrites the current value of this record's `roles` field.
+	SetRoles([]dlib.Snowflake) AllowedMentions
+
+	// Users returns the current value of this record's `users` field.
+	//
+	// The `users` field contains an array of user_ids to mention (Max size of
+	// 100).
+	Users() []dlib.Snowflake
+
+	// SetUsers overwrites the current value of this record's `users` field.
+	SetUsers([]dlib.Snowflake) AllowedMentions
+}
