6ed02e890a5a3769519e9727139476a909c96a07
Elizabeth Harper <foxcapade@gmail.com>
2020-10-18 13:32:39 -0400

    added explicit content filter level enum

---
+++ pkg/discord/guild/ExplicitContentFilterLevel.go
@@ -0,0 +1,28 @@
+package guild
+
+import "errors"
+
+var (
+	ErrBadExpConFilterLvl = errors.New("unrecognized explicit content filter level value")
+)
+
+type ExplicitContentFilterLevel uint8
+
+const (
+	ExpConFilterLvlDisabled ExplicitContentFilterLevel = iota
+	ExpConFilterLvlMembersWithoutRoles
+	ExpConFilterLvlAllMembers
+)
+
+func (e ExplicitContentFilterLevel) IsValid() bool {
+	return nil == e.Validate()
+}
+
+func (e ExplicitContentFilterLevel) Validate() error {
+	if e > ExpConFilterLvlAllMembers {
+		return ErrBadExpConFilterLvl
+	}
+
+	return nil
+}
+
