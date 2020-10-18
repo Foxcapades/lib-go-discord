3718985c2ae9ed674688b04c1fa602a4d6d5f920
Elizabeth Harper <foxcapade@gmail.com>
2020-10-18 16:26:47 -0400

    added allowed mention type enum

---
+++ pkg/discord/message/AllowedMentionType.go
@@ -0,0 +1,34 @@
+package message
+
+import "errors"
+
+var (
+	ErrBadAllowedMentionType = errors.New("unrecognized allowed mention type value")
+)
+
+type AllowedMentionType string
+
+const (
+	// Controls role mentions.
+	AllowedMentionTypeRoles    AllowedMentionType = "roles"
+
+	// Controls user mentions.
+	AllowedMentionTypeUsers    AllowedMentionType = "users"
+
+	// Controls @everyone and @here mentions.
+	AllowedMentionTypeEveryone AllowedMentionType = "everyone"
+)
+
+func (a AllowedMentionType) IsValid() bool {
+	return nil == a.Validate()
+}
+
+func (a AllowedMentionType) Validate() error {
+	switch a {
+	case AllowedMentionTypeRoles, AllowedMentionTypeUsers, AllowedMentionTypeEveryone:
+		return nil
+	}
+
+	return ErrBadAllowedMentionType
+}
+
