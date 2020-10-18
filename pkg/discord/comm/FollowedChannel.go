0f985f75daae35cf00e207ccfd1c0d9b0e812d76
Elizabeth Harper <foxcapade@gmail.com>
2020-10-18 15:22:26 -0400

    updated the followed channel interface documentation

--- pkg/discord/comm/FollowedChannel.go
+++ pkg/discord/comm/FollowedChannel.go
@@ -5,11 +5,22 @@ import "github.com/foxcapades/lib-go-discord/pkg/dlib"
 // FollowedChannel
 // TODO: Document me
 type FollowedChannel interface {
-	// source channel id
+	// ChannelID returns the current value of this record's `channel_id` field.
+	//
+	// The `channel_id` field contains the current record's source channel id.
 	ChannelID() dlib.Snowflake
+
+	// SetChannelID overwrites the current value of this record's `channel_id`
+	// field.
 	SetChannelID(dlib.Snowflake) FollowedChannel
 
-	// created target webhook id
+	// WebhookID returns the current value of this record's `webhook_id` field.
+	//
+	// The `webhook_id` field contains the current record's created target webhook
+	// id.
 	WebhookID() dlib.Snowflake
+
+	// SetWebhookID overwrites the current value of this record's `webhook_id`
+	// field.
 	SetWebhookID(dlib.Snowflake) FollowedChannel
 }
