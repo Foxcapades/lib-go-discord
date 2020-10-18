edbdeae99be3e42a227b31afeddf84a50333b8e9
Elizabeth Harper <foxcapade@gmail.com>
2020-10-18 13:30:17 -0400

    added message notification level enum

---
+++ pkg/discord/guild/MessageNotificationLevel.go
@@ -0,0 +1,26 @@
+package guild
+
+import "errors"
+
+var (
+	ErrBadMsgNoteLevel = errors.New("unrecognized message notification level value")
+)
+
+type MessageNotificationLevel uint8
+
+const (
+	MsgNoteLvlAllMessages MessageNotificationLevel = iota
+	MsgNoteLvlOnlyMentions
+)
+
+func (m MessageNotificationLevel) IsValid() bool {
+	return nil == m.Validate()
+}
+
+func (m MessageNotificationLevel) Validate() error {
+	if m > MsgNoteLvlOnlyMentions {
+		return ErrBadMsgNoteLevel
+	}
+
+	return nil
+}
